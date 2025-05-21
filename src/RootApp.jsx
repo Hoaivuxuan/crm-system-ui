ad6c6d8d19ea3742c879ca6c321766d040573168
Vu Xuan Hoai <86643318+Hoaivuxuan@users.noreply.github.com>
2025-05-21 10:51:51 +0700

    update core

---
+++ src/RootApp.jsx
@@ -0,0 +1,21 @@
+import './style/app.css';
+
+import { Suspense, lazy } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '@/redux/store';
+import PageLoader from '@/components/PageLoader';
+
+const CrmOs = lazy(() => import('./apps/CrmOs'));
+
+export default function RoutApp() {
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <Suspense fallback={<PageLoader />}>
+          <CrmOs />
+        </Suspense>
+      </Provider>
+    </BrowserRouter>
+  );
+}
